03cacc736952e6a5d37e5d95408bd8874a9f489b
Marek Ciążyński <m.ciazynski@bfirst.tech>
2020-04-13 18:57:20 +0200

    Add simple gravity

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 'use strict';
 import './style.css'
 
-let x;
+const GRTAVITY = 0.33;
+const JUMP_VELOCITY = 6;
+const HEIGHT = 640;
+
+const _bird = {
+	domElement: null,
+	velocity: 0,
+	position: 0,
+};
+
+const _animation = {
+	handler: null,
+	lastTimestamp: 0,
+};
+
 
 function jump() {
 	console.log('jump')
+	_bird.velocity = -JUMP_VELOCITY
+}
+
+function animate(timestamp) {	
+	const timeDelta = timestamp - _animation.lastTimestamp;
+	const moveFactor = (timeDelta / 16); // requestanimationframe defaults to 60 FPS (16ms)
+
+	console.log(timeDelta);
+	_bird.position += _bird.velocity * moveFactor;
+	_bird.velocity += GRTAVITY;
+	_bird.domElement.style.transform = `translateY(${_bird.position}px)`;
+	console.log(_bird)
+
+	_animation.lastTimestamp = timestamp;
+	if (_bird.position <= HEIGHT)
+		_animation.handler = requestAnimationFrame(animate);
 }
 
 // Main
 document.addEventListener("DOMContentLoaded", function (event) {
-	console.log('DOMload')
+	_bird.domElement = document.getElementById('bird');
+	console.log('DOMload', _bird)
 
 	document.addEventListener('keydown', (event) => {
 		if (event.repeat)
@@ -19,4 +50,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 			jump();
 		}
 	});
+
+	_animation.handler = requestAnimationFrame(animate);
 });
