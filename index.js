2a9a6310c09bde350293353aa1f5adf94ee2240e
Marek Ciążyński <m.ciazynski@bfirst.tech>
2020-04-13 18:21:02 +0200

    Add basic layout and assets

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 'use strict';
 import './style.css'
 
+let x;
+
+function jump() {
+	console.log('jump')
+}
+
 // Main
 document.addEventListener("DOMContentLoaded", function (event) {
-	
+	console.log('DOMload')
+
+	document.addEventListener('keydown', (event) => {
+		if (event.repeat)
+			return false;
+		
+		if ([' ', 'Enter'].includes(event.key)) {
+			jump();
+		}
+	});
 });
